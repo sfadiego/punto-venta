ef67bf300489398c159d90d26e2262a497117955
Diego Silva <sfadiego@gmail.com>
2026-07-20 19:32:17 -0600

    fix: arregla redireccionamiento de login causado por landing page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->file(public_path('landing/index.html'));
-});
-
 Route::get('/{any}', function () {
     return response(view('index'))
         ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
