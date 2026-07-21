7024246785b263008a2c444903039d333d26da7a
Diego Silva <sfadiego@gmail.com>
2026-07-20 18:28:26 -0600

    feat: pagina

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->file(public_path('landing/index.html'));
+});
+
 Route::get('/{any}', function () {
     return response(view('index'))
         ->header('Cache-Control', 'no-store, no-cache, must-revalidate');
